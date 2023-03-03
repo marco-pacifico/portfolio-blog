9171495d5067c1631eb4331adc7ba2c7c383db7b
Marco Pacifico <marco.pacifico@gmail.com>
2023-03-03 00:03:44 -0600

    more work section

--- components/sections/MoreWork.js
+++ components/sections/MoreWork.js
@@ -1,20 +1,21 @@
 import styled from "styled-components";
-import ListItem from "../ui/ListItem.js";
-import POSTS from "../../data/POSTS.js";
+import Card from "../ui/Card";
+import Grid from "../ui/Grid";
+import PROJECTS from "../../data/PROJECTS";
 import { Overline } from "../ui/Typography.js";
 import { BREAKPOINT } from "../../styles/1-DesignTokens.js";
-
+import firstimage from "../../pages/writing/post-one/image-one.jpg";
 
 const MoreWork = ({ currentPost }) => {
-  const filteredPOSTS = POSTS.filter((post) => post.slug !== currentPost);
+  const filteredPROJECTS = PROJECTS.filter((project) => project.slug !== currentPost);
   return (
     <MorePostsSection>
       <Overline>More Work</Overline>
-      <ol>
-        {filteredPOSTS.map(({ slug, title, description }) => (
-          <ListItem key={slug} href={`/writing/${slug}`} title={title} description={description} />
+      <Grid>
+        {filteredPROJECTS.map(({ slug, title, description }) => (
+          <Card key={slug} href={`/work/${slug}`} title={title} description={description} image={firstimage}/>
         ))}
-      </ol>
+      </Grid>
     </MorePostsSection>
   );
 };
@@ -22,13 +23,24 @@ const MoreWork = ({ currentPost }) => {
 export default MoreWork;
 
 const MorePostsSection = styled.section`
-  margin-top: var(--space-9); 
-  padding-top: var(--space-9);
-  border-top: 1px solid var(--color-border);
-  padding-left: var(--section-offset);
-  padding-right: var(--section-offset);
+  --list-item-space-min: var(--space-7);
+  --list-item-space-target: var(--space-fluid-8);
+  --list-item-space-max: var(--space-fluid-8);
+  --list-item-space: clamp(
+    var(--list-item-space-min),
+    var(--list-item-space-target),
+    var(--list-item-space-max)
+  );
+  display: flex;
+  flex-direction: column;
+  gap: var(--list-item-space);
 
+  margin-top: var(--space-9); 
+  border-top: 1px solid var(--color-border);
+  padding-top: var(--space-9);
+  padding-inline: var(--section-offset);
   padding-bottom: var(--space-12);
+
   max-width: ${BREAKPOINT.desktoplarge / 16}rem;
   margin-inline: auto;
   /* margin-left: var(--section-offset); */
