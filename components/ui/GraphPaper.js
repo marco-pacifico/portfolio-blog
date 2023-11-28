c1ae9d264eedcb1933577f56c48001391468966e
Marco Pacifico <marco.pacifico@gmail.com>
2023-11-28 08:29:53 -0500

    static decorative squares

--- components/ui/GraphPaper.js
+++ components/ui/GraphPaper.js
@@ -5,8 +5,8 @@ export default function GraphPaper({ children, ...props }) {
   return (
     <Background {...props}>
       <Glow aria-hidden />
-      <Grid />
-        {children}
+      <GridLines />
+      {children}
     </Background>
   );
 }
@@ -35,15 +35,14 @@ const Glow = styled.div`
   background-image: radial-gradient(
     circle at 50% 50%,
     var(--color-glow) 0%,
-    // TODO: make this a prop
     transparent 40%
-  );
+  ); // TODO: make this a prop
   opacity: var(--glow-opacity); // TODO: make this a prop
   height: var(--glow-height); // TODO: make this a prop
   width: var(--glow-width); // TODO: make this a prop
 `;
 
-function Grid({ ...props }) {
+function GridLines({ ...props }) {
   return (
     <StyledGridLines>
       <defs>
@@ -56,14 +55,15 @@ function Grid({ ...props }) {
           patternUnits="userSpaceOnUse"
         >
           <path
-            d="M 10 10 V.5 M0 .5 H10"
+            d="M 5 10 V.5 M0 .5 H10"
             // d={`M${gridWidth / 2} ${gridHeight} V.5 M0 .5 H${gridWidth}`} // Make a props
             fill="none"
+            // strokeWidth={1} // If you want to make this 0.5, need to change y-offset to -0.5, and change path to "M 5 10 V0 M0 0 H10" and then also have to adjust positioning of the decorative squares
           />
         </pattern>
-        {/* TODO Add Squares */}
       </defs>
-      <rect width="100%" height="100%" fill="url(#grid)" />
+      <DecorativeSquares />
+      <rect width="100%" height="100%" fill="url(#grid)" strokeWidth={0} />
     </StyledGridLines>
   );
 }
@@ -85,4 +85,37 @@ const StyledGridLines = styled.svg`
     rgba(255, 255, 255, 0.05),
     transparent
   ); // TODO: make second percentage a prop mask coverage
+`;
+
+function DecorativeSquares({ ...props }) {
+  const gridWidth = 10; // TODO: make this a prop
+  const gridHeight = 10; // TODO: make this a prop
+  const squareColor = "purple"; // TODO: make this a prop
+  const squareOpacity = 0.5; // TODO: make this a prop
+  return (
+    <StyledDecorativeSquares
+      x="50%"
+      y={-1}
+      style={{ fill: squareColor, opacity: squareOpacity }}
+    >
+      <path
+        d={`M-${gridWidth / 2 + 0.5} 0 
+                h${gridWidth + 1} v${gridHeight + 1} h-${gridWidth + 1}Z 
+                M${gridWidth * 3.5 - 0.5} 0 h${gridWidth + 1} v${
+          gridHeight + 1
+        } h-${gridWidth + 1}Z 
+                M${gridWidth * 1.5 - 0.5} ${gridHeight * 2} h${
+          gridWidth + 1
+        } v${gridHeight + 1} h-${gridWidth + 1}Z  
+                M-${gridWidth * 1.5 + 0.5} ${gridHeight * 3} h${
+          gridWidth + 1
+        } v${gridHeight + 1} h-${gridWidth + 1}Z  `}
+        strokeWidth={0}
+      />
+    </StyledDecorativeSquares>
+  );
+}
+
+const StyledDecorativeSquares = styled.svg`
+  overflow: visible;
 `;
