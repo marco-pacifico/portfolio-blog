39a3c0e44907c21080017deb4de8f572d57393f3
Marco Pacifico <marco.pacifico@gmail.com>
2023-11-30 08:29:00 -0500

    glow z-index switch

--- pages/testing/index.js
+++ pages/testing/index.js
@@ -5,77 +5,75 @@ import Slider from "../../components/ui/Slider";
 import GraphPaper from "../../components/ui/GraphPaper";
 
 export default function Testing() {
-  const [checked, setChecked] = React.useState(false);
   const [cellSize, setCellSize] = React.useState(50);
   const [gridOpacity, setGridOpacity] = React.useState(50);
   const [glowOpacity, setGlowOpacity] = React.useState(50);
   const [glowHeight, setGlowHeight] = React.useState("800");
   const [glowWidth, setGlowWidth] = React.useState("1100");
+  const [glowOnTop, setGlowOnTop] = React.useState(false);
 
   return (
     <Wrapper>
-      <GraphPaper
-        cellSize={cellSize}
-        gridOpacity={gridOpacity}
-        glowOpacity={glowOpacity}
-        glowWidth={glowWidth}
-        glowHeight={glowHeight}
-      >
-        {" "}
-        {/* Pass the cellSize state prop to the GraphPaper component */}
-        <Controls>
-          <Switch
-            checked={checked}
-            onChange={(e) => setChecked(e.target.checked)}
-          >
-            Switch is {checked ? "on" : "off"}
-          </Switch>
-          <Slider
-            id="cellSize"
-            label="Cell Size"
-            value={cellSize} // Pass the cellSize to the value prop of Slider component
-            units="px"
-            min={10}
-            max={400}
-            step={10}
-            onChange={(e) => setCellSize(+e.target.value)} //+e.target.value converts input string to number
-          />
-          <Slider
-            id="gridOpacity"
-            label="Grid Opacity"
-            value={gridOpacity} // Pass the cellSize to the value prop of Slider component
-            units="%"
-            onChange={(e) => setGridOpacity(+e.target.value)} //+e.target.value converts input string to number
-          />
-          <Slider
-            id="glowOpacity"
-            label="Glow Opacity"
-            value={glowOpacity} // Pass the cellSize to the value prop of Slider component
-            units="%"
-            onChange={(e) => setGlowOpacity(+e.target.value)} //+e.target.value converts input string to number
-          />
-          <Slider
-            id="glowWidth"
-            label="Glow Width"
-            value={glowWidth} // Pass the cellSize to the value prop of Slider component
-            units="px"
-            min={0}
-            max={2000}
-            step={20}
-            onChange={(e) => setGlowWidth(+e.target.value)} //+e.target.value converts input string to number
-          />
-          <Slider
-            id="glowHeight"
-            label="Glow Height"
-            value={glowHeight} // Pass the cellSize to the value prop of Slider component
-            units="px"
-            min={0}
-            max={1200}
-            step={20}
-            onChange={(e) => setGlowHeight(+e.target.value)} //+e.target.value converts input string to number
-          />
-        </Controls>
-      </GraphPaper>
+        <GraphPaper
+          cellSize={cellSize}
+          gridOpacity={gridOpacity}
+          glowOpacity={glowOpacity}
+          glowWidth={glowWidth}
+          glowHeight={glowHeight}
+          glowOnTop={glowOnTop}
+        />
+      <Controls>
+        <Switch
+          checked={glowOnTop}
+          onChange={(e) => setGlowOnTop(e.target.checked)}
+        >
+          Glow {glowOnTop ? "on top" : "behind"}
+        </Switch>
+        <Slider
+          id="cellSize"
+          label="Cell Size"
+          value={cellSize} // Pass the cellSize to the value prop of Slider component
+          units="px"
+          min={10}
+          max={400}
+          step={10}
+          onChange={(e) => setCellSize(+e.target.value)} //+e.target.value converts input string to number
+        />
+        <Slider
+          id="gridOpacity"
+          label="Grid Opacity"
+          value={gridOpacity} // Pass the cellSize to the value prop of Slider component
+          units="%"
+          onChange={(e) => setGridOpacity(+e.target.value)} //+e.target.value converts input string to number
+        />
+        <Slider
+          id="glowOpacity"
+          label="Glow Opacity"
+          value={glowOpacity} // Pass the cellSize to the value prop of Slider component
+          units="%"
+          onChange={(e) => setGlowOpacity(+e.target.value)} //+e.target.value converts input string to number
+        />
+        <Slider
+          id="glowWidth"
+          label="Glow Width"
+          value={glowWidth} // Pass the cellSize to the value prop of Slider component
+          units="px"
+          min={0}
+          max={2000}
+          step={20}
+          onChange={(e) => setGlowWidth(+e.target.value)} //+e.target.value converts input string to number
+        />
+        <Slider
+          id="glowHeight"
+          label="Glow Height"
+          value={glowHeight} // Pass the cellSize to the value prop of Slider component
+          units="px"
+          min={0}
+          max={1200}
+          step={20}
+          onChange={(e) => setGlowHeight(+e.target.value)} //+e.target.value converts input string to number
+        />
+      </Controls>
     </Wrapper>
   );
 }
@@ -83,11 +81,16 @@ export default function Testing() {
 const Wrapper = styled.div`
   padding: var(--space-8);
   height: 80vh;
+  position: relative;
 `;
 
 const Controls = styled.div`
+  background-color: rgba(255, 255, 255, 0.2);
+  padding: var(--space-6);
+  border-radius: var(--space-4);
+  isolation: isolate; // Creates new stacking context and prevents the controls from being clipped by the GraphPaper
+  max-width: 200px;
+  margin: auto;
   display: grid;
-  place-content: center;
-  gap: var(--space-8);
-  height: 100%;
+  gap: var(--space-6);
 `;
