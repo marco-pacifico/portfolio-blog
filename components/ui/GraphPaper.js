222f56ab0c32f96212bbe3919851b2cbbdb405ee
Marco Pacifico <marco.pacifico@gmail.com>
2023-12-09 17:00:11 -0500

    add height to svg wrapping decorative squares

--- components/ui/GraphPaper.js
+++ components/ui/GraphPaper.js
@@ -33,6 +33,7 @@ export default function GraphPaper({
             position: "absolute",
             inset: 0,
             width: "100%",
+            height: "100%",
           }}
         >
           <DecorativeSquares cellSize={cellSize} />
